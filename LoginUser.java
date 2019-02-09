684c9ae1a0332dc10240c3c7056862d53dd642b1
pinaknath <pinak.nath1@outlook.com>
2019-02-09 22:09:24 +0530

    file name copied and slightly chnaged

---
+++ LoginUser.java
@@ -0,0 +1,23 @@
+
+
+
+def print_factors(x)
+
+	print("The factors of" ,x,"are:")
+
+	for i in range(1, x+1):
+
+		if x% i==0:
+
+		print(i)
+
+	num = 320
+
+	print_factors(num)
+
+
+	if else x= 65
+	then print(all numbers are same)
+
+	the new line added
+
