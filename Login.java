600ad6a90863acc3d2b4951dc019dd5c5453904b
pinaknath <pinak.nath1@outlook.com>
2019-02-13 08:18:12 +0530

    send it to repository

--- Login.java
+++ Login.java
@@ -1,23 +1,7 @@
+new file created by using mv command
 
+which is really possible and interesting
 
+Lets send it to repository
 
-def print_factors(x)
-
-	print("The factors of" ,x,"are:")
-
-	for i in range(1, x+1):
-
-		if x% i==0:
-
-		print(i)
-
-	num = 320
-
-	print_factors(num)
-
-
-	if else x= 65
-	then print(all numbers are same)
-
-	the new line added
 
