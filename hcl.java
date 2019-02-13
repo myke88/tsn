23e9d7c4c0d45aff358693732b4b8467a3e20765
pinaknath <pinak.nath1@outlook.com>
2019-02-13 21:48:37 +0530

    added hcl.java file with explanation

---
+++ hcl.java
@@ -0,0 +1,9 @@
+The new java login is added and hence there were new code is added
+
+Thecode added to test whetehr the file is present in the Master branch or not.
+
+This step is taken to prove that the branch takes all the snapshot of the Master branch.
+
+After commiting the file should not be there in the branch .
+
+The file which is created in the Master will be committed to the new branch which is develop by name
